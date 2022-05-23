4d092ccabcac65f54c13a0d583e94dbd3bb48c5d
Pushplata <pushplata.de@gmail.com>
2022-05-23 15:26:12 +0200

    first push

--- src/test/java/runner/TestRunnerCucumber.java
+++ src/test/java/runner/TestRunnerCucumber.java
@@ -1,7 +1,5 @@
 package runner;
 
-import org.testng.annotations.DataProvider;
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
@@ -17,7 +15,6 @@ import io.cucumber.testng.CucumberOptions;
 //runner class to execute all scenarios 
 public class TestRunnerCucumber extends AbstractTestNGCucumberTests {
 	@Override
-	@DataProvider()
 	public Object[][] scenarios() {
 		return super.scenarios();
 	}
